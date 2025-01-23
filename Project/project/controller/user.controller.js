055482e32cad16a810fba25a02e8a7cf2b74c026
LuiisPaulo <luispaulolopesg@gmail.com>
2025-01-23 01:12:00 -0300

    Commitando

---
+++ Project/project/controller/user.controller.js
@@ -0,0 +1,7 @@
+const userModel = require('../models/user.models');
+
+
+
+exports.createUser = async (req, res) => {
+
+}
---
+++ Project/project/models/purchaseTicket.models.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const purchaseTicketSchema = new mongoose({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true 
+    },
+
+    tickets:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Ticket',
+        required: true,
+        unique: true 
+    },
+    quantity:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+
+    timestamps: true,
+});
+
+module.exports = purchaseTicketSchema('purchaseTicket', purchaseTicketSchema);
