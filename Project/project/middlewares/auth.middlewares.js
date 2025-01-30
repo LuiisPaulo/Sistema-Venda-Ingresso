96f74b6f5c6d819a9c21da4065c3f3022c651ab5
LuiisPaulo <luispaulolopesg@gmail.com>
2025-01-30 00:08:37 -0300

    Modificando teste

--- Project/project/middlewares/auth.middlewares.js
+++ Project/project/middlewares/auth.middlewares.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.models");
 
 const validation = (req, res, next) => {
-    const token = req.headers.authorization;
+    const token = req.headers("Autorizado").replace("Bearer ", "");
     if (!token){
         return res.status(401).json({ message: "Token não encontrado" });
     }
