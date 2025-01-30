d7a8438f51455df014c9c3def1d6734327dfbf2b
LuiisPaulo <luispaulolopesg@gmail.com>
2025-01-30 00:04:09 -0300

    Criando classe de autenticacao

--- Project/project/middlewares/auth.middlewares.js
+++ Project/project/middlewares/auth.middlewares.js
@@ -1,5 +1,25 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.models");
 
-exports.auth = async (req, res, next) => {
-    const token = 
+const validation = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token){
+        return res.status(401).json({ message: "Token não encontrado" });
+    }
+
+    try {
+        const decode = jwt.verify(token, "secret");
+        req.user = decode; 
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Erro ao validar o token" });
+    }
+};
+
+exports.isAdmin = async (req, res, next) => {
+    const user = await userModel.findById(req.user.userId);
+    if (user.role !== "admin") {
+        return res.status(401).json({ message: "Usuário não autorizado" });
+    }
+    next();
+};
