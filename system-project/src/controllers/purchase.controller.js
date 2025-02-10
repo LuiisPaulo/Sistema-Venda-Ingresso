5ba3a55554f3e6e62056d98e67258827c8ff2075
LuiisPaulo <luispaulolopesg@gmail.com>
2025-02-10 08:43:09 -0300

    Fix: criando purchase.controller

--- system-project/src/controllers/purchase.controller.js
+++ system-project/src/controllers/purchase.controller.js
@@ -1,7 +1,46 @@
 import purchaseModel from '../models/purchase.model';
+import ticketModel from '../models/ticket.models';
 
 // TODO -> criar função de compra
+exports.buyTicket = async (req, res) => {
+    const { ticketId, quantity } = req.body;
+    const ticket = await ticketModel.findById(ticketId);
+    try{
+        if(!ticket || !quantity){
+            return res.status(404).json({ message: "Erro ao encontrar o ticket" });
+        }
+
+        if(ticket.quantity < quantity){
+            return res.status(400).json({ message: "Quantidade insuficiente" });
+        }
+
+        ticket.quantity -= quantity;
+        await ticket.save();
+
+        const purchase = await purchaseModel.create({
+            user: req.user._id,
+            ticket: ticket._id,
+            quantity,
+        });
+
+        res.redirect('/history');
+
+    }catch(err){
+        return res.status(500).json({error: 'Erro ao realizar a compra'});
+    } 
+};
+
+
 // TODO -> criar função para verificar historico de compra
-// TODO -> criar função para mostar para usuario as compras realizadas
-
-
+exports.viewPurchase = async (req, res) => {
+    const { id } = req.params;
+    try{
+        const purchase = await purchaseModel.find({ user: req.user._id, id }).populate('ticket');
+        if(!purchase){
+            return res.status(404).json({ message: "Erro ao encontrar a compra" });
+        }
+        res.reder('/history', { purchase });
+    }catch(err){
+        return res.status(500).json({error: 'Erro ao visualizar a compra'});
+    }; 
+};
