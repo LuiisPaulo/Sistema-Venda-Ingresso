6356cc74a344004e6d14930a2923adc00b9ce18b
LuiisPaulo <luispaulolopesg@gmail.com>
2025-02-11 19:14:32 -0300

    Fix: add database.js

--- system-project/src/config/database.js
+++ system-project/src/config/database.js
@@ -1,1 +1,21 @@
 // TODO -> criar arquivo de configuração de banco de dados
+// TODO -> refazer sozinho essa classe 
+import mongoose from 'mongoose';
+require('dotenv').config();
+
+const connectDB = async () => {
+    try{
+        const connection = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        console.log('Conectado com sucesso ao banco de dados');
+        return connection;
+    }catch(err){
+        console.log('Erro ao conectar ao banco de dados');
+        throw err;
+    }
+};
+
+export default connectDB;
